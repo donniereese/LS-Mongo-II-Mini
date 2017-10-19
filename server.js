c686e387f715d58a962543a6b3db69e35e0bcd1c
Donnie Reese <donniereese@gmail.com>
2017-10-18 21:58:35 -0500

    Major tasks done, need optimizing of mongoose methods

--- server.js
+++ server.js
@@ -15,6 +15,58 @@ server.use(bodyParser.json());
 
 // Your API will be built out here.
 
+server.get('/users', (req, res) => {
+    Person.find().then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.status(STATUS_SERVER_ERROR).json(err);
+    });
+
+});
+
+server.get('/users/:direction', (req, res) => {
+    // if (!req.body.direction) res.status(STATUS_USER_ERROR).json({error: 'Request did not specify sort \'direction\''});
+    if (['asc', 'desc'].indexOf(req.params.direction) === -1) {
+        res.status(STATUS_USER_ERROR).json({ error: 'direction type unsupported' });
+    }
+
+    Person.find()
+    .sort({firstName: req.params.direction === 'asc' ? 1 : -1 })
+    .then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    });
+});
+
+server.get('/user-get-friends/:id', (req, res) => {
+    Person.findOne({ '_id': req.params.id }).select('friends').then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    });
+});
+
+server.put('/users', (req, res) => {
+    if (!req.body.user || !req.body.query) {
+        return res.status(STATUS_USER_ERROR).json({ error: 'Did not supply single user object or array of user objects' });
+    }
+
+    const findArr = Object.keys(req.body.query).map((key, i) => {
+        return {key: req.body.query[key]};
+    });
+
+    if (findArr.length === 0) {
+        return res.status(STATUS_USER_ERROR).json({ error: 'Did not provide proper query object' });
+    }
+
+    Person.findOneAndUpdate(req.body.query, req.body.user).then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.status(STATUS_SERVER_ERROR).json({ error: err });
+    });
+});
+
 mongoose.Promise = global.Promise;
 const connect = mongoose.connect('mongodb://localhost/people', {
   useMongoClient: true
